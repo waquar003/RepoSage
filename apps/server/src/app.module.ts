806fe8ba60d3e5d5805d4be501ff3d76fc753125
Waquar Ahmad <waquar.ahmad4321@gmail.com>
2026-03-01 12:25:26 +0530

    feat(db): extract prisma to shared package and implement nestjs database module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StorageModule } from './storage/storage.module';
 import { TranscriptionModule } from './transcription/transcription.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [StorageModule, TranscriptionModule],
+  imports: [StorageModule, TranscriptionModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
